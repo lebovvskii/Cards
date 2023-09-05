7390d40f6fbd746d7a6cf1332029cf2a99c122dc
Nikita Lebovvskii <lebovvskii.design@ya.ru>
2023-09-05 17:18:18 +1000

    optimized renderCards

--- cards/script.js
+++ cards/script.js
@@ -6,16 +6,15 @@ async function getProducts() {
 }
 
 const renderCards = (products) => {
-    for (let i = 0; i < products.length; i++) {
-        const section = document.getElementById("cards-section")
-        section.innerHTML += `
-            <div class="product-card">
-                <h2 class="product-card__title">${products[i].title}</h2>
-                <img class="product_card__img" src="${products[i].image}" />
-                <p class="product-card__description">${products[i].description}</p>
-            </div>
-        `;
-    }
+    const section = document.getElementById("cards-section")
+    const cardElems = products.map(product => `
+        <div class="product-card">
+            <h2 class="product-card__title">${product.title}</h2>
+            <img class="product_card__img" src="${product.image}"/>
+            <p class="product-card__description">${product.description}</p>
+        </div>
+    `)
+    section.innerHTML = cardElems.join("");
 }
 
 const init = async () => {
